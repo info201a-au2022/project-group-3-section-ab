69c4d906b32c3b5bd62ad236748a4d041ccb2df6
Kristy Nhan <kristynh@cs.washington.edu>
2022-11-16 06:18:21 -0800

    Dataset 3 graph first draft

--- source/dataset3_graph.R
+++ source/dataset3_graph.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(tidyverse)
 library(stringr)
 library(readr)
+library(ggplot2)
 
 # load dataset3
 data3 <- read.csv("../data/Driving Times to Abortion Clinics in the US.csv")
@@ -49,3 +50,32 @@ ca3 <- data3 %>%
               calculate_mean(gestation_20_duration, filter_ca(gestation_20_duration)),
             !!generate_title(20, nrow(filter_ca(gestation_20_duration_closed))) :=  
               calculate_mean(gestation_20_duration_closed, filter_ca(gestation_20_duration_closed)))
+
+weeks_in_pregnancy <- c(8, 12, 16, 20, 8, 12, 16, 20)
+
+avg_driving_time <- c(calculate_mean(gestation_8_duration, filter_ca(gestation_8_duration)),
+                     calculate_mean(gestation_12_duration, filter_ca(gestation_12_duration)),
+                     calculate_mean(gestation_16_duration, filter_ca(gestation_16_duration)),
+                     calculate_mean(gestation_20_duration, filter_ca(gestation_20_duration)))
+
+avg_driving_time2 <- c(calculate_mean(gestation_8_duration_closed, filter_ca(gestation_8_duration_closed)),
+                      calculate_mean(gestation_12_duration_closed, filter_ca(gestation_12_duration_closed)),
+                      calculate_mean(gestation_16_duration_closed, filter_ca(gestation_16_duration_closed)),
+                      calculate_mean(gestation_20_duration_closed, filter_ca(gestation_20_duration_closed)))
+
+df = data.frame(weeks_in_pregnancy, avg_driving_time, avg_driving_time2)
+
+df <- data.frame(supp=rep(c("Closest", "Second closest"), each=4),
+                  weeks_in_pregnancy=rep(c(8, 12,16, 20),2),
+                  avg_driving_times=c(calculate_mean(gestation_8_duration, filter_ca(gestation_8_duration)),
+                                      calculate_mean(gestation_12_duration, filter_ca(gestation_12_duration)),
+                                      calculate_mean(gestation_16_duration, filter_ca(gestation_16_duration)),
+                                      calculate_mean(gestation_20_duration, filter_ca(gestation_20_duration)),
+                                      calculate_mean(gestation_8_duration_closed, filter_ca(gestation_8_duration_closed)),
+                                      calculate_mean(gestation_12_duration_closed, filter_ca(gestation_12_duration_closed)),
+                                      calculate_mean(gestation_16_duration_closed, filter_ca(gestation_16_duration_closed)),
+                                      calculate_mean(gestation_20_duration_closed, filter_ca(gestation_20_duration_closed))))
+
+plot <- ggplot(data=df, aes(x=weeks_in_pregnancy, y=avg_driving_times, group=supp)) +
+  geom_line(aes(color=supp))+
+  geom_point(aes(color=supp))
